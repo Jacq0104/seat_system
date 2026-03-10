55e3aa4c3edea0c1310fd861f36689f9df90c38c
Jacq <euv0104@gmail.com>
2026-03-10 15:33:07 +0800

    add ddl, dml

---
+++ DB/dml.sql
@@ -0,0 +1,23 @@
+INSERT INTO SeatingChart(floor_no, seat_no) VALUES (1,'1');
+INSERT INTO SeatingChart(floor_no, seat_no) VALUES (1,'2');
+INSERT INTO SeatingChart(floor_no, seat_no) VALUES (1,'3');
+INSERT INTO SeatingChart(floor_no, seat_no) VALUES (1,'4');
+
+INSERT INTO SeatingChart(floor_no, seat_no) VALUES (2,'1');
+INSERT INTO SeatingChart(floor_no, seat_no) VALUES (2,'2');
+INSERT INTO SeatingChart(floor_no, seat_no) VALUES (2,'3');
+INSERT INTO SeatingChart(floor_no, seat_no) VALUES (2,'4');
+
+INSERT INTO SeatingChart(floor_no, seat_no) VALUES (3,'1');
+INSERT INTO SeatingChart(floor_no, seat_no) VALUES (3,'2');
+INSERT INTO SeatingChart(floor_no, seat_no) VALUES (3,'3');
+INSERT INTO SeatingChart(floor_no, seat_no) VALUES (3,'4');
+
+INSERT INTO SeatingChart(floor_no, seat_no) VALUES (4,'1');
+INSERT INTO SeatingChart(floor_no, seat_no) VALUES (4,'2');
+INSERT INTO SeatingChart(floor_no, seat_no) VALUES (4,'3');
+INSERT INTO SeatingChart(floor_no, seat_no) VALUES (4,'4');
+
+INSERT INTO Employee VALUES ('26001','王一','wang1@mail.com',NULL);
+INSERT INTO Employee VALUES ('26002','劉二','liou2@mail.com',NULL);
+INSERT INTO Employee VALUES ('26003','王二','wang3@mail.com',NULL);
