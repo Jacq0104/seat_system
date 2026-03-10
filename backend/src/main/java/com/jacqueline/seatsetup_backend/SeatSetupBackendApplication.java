e44a81dfd57460dab955c52f01f34018128e00f1
Jacq <euv0104@gmail.com>
2026-03-10 11:20:33 +0800

    init

---
+++ backend/src/main/java/com/jacqueline/seatsetup_backend/SeatSetupBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jacqueline.seatsetup_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatSetupBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeatSetupBackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/jacqueline/seatsetup_backend/SeatSetupBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacqueline.seatsetup_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatSetupBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
